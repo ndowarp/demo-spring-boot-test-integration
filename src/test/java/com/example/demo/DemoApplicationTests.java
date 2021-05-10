54b695f39ebecb0a4e61838b36f9d981aa815528
ndowarp <nicolas961945@gmail.com>
2021-05-10 14:08:36 -0500

    Added repository test
    Test using jUnit5 and assertj

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		//given
+		int n1 = 20;
+		int n2 = 30;
+		//when
+		int result  = underTest.add(n1, n2);
+		//then
+		int expected  = 50;
+		assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
 	}
 
 }
